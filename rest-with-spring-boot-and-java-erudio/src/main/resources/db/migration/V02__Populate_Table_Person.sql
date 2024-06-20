120a09ca7465d2c3e99f1f0d6019717e3608244b
TiagoTSR <craimos3001@gmail.com>
2024-06-20 13:59:27 -0300

    Adicionando migrations Flyway

---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V02__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Aryton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'Da Vinci'),
+	(5, 'Anchiano dgegs455455segsegseg - Italy', 'Leonardo45', 'Male', 'Da Vinci545'),
+	(6, 'Mvezo - south africa', 'Nelson', 'Male', 'Mandela');
