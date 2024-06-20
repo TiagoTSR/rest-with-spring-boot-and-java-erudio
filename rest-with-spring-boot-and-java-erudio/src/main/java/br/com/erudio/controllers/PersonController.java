fccc326d9eabacbc61bbdd0b045a8b84e8c53bce
TiagoTSR <craimos3001@gmail.com>
2024-06-20 08:27:34 -0300

    Versionamento de api

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -23,6 +24,7 @@ public class PersonController {
 			
 	@Autowired
 	private PersonServices service;
+	
 		
 	@GetMapping(produces =  MediaType.APPLICATION_JSON_VALUE)
 	public List<PersonVO> findAll(){ 
@@ -37,7 +39,12 @@ public class PersonController {
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces =  MediaType.APPLICATION_JSON_VALUE)
 	public PersonVO create(@RequestBody PersonVO person){ 
 	    return service.create(person);
-	}     
+	}  
+	
+	@PostMapping(value = "/v2" ,consumes = MediaType.APPLICATION_JSON_VALUE,produces =  MediaType.APPLICATION_JSON_VALUE)
+	public PersonVOV2 createV2(@RequestBody PersonVOV2 person){ 
+	    return service.createV2(person);
+	}    
 		
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces =  MediaType.APPLICATION_JSON_VALUE)
 	public PersonVO update(@RequestBody PersonVO person){ 
