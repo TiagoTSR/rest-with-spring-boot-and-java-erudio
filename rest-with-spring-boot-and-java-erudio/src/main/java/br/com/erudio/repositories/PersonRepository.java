663ded531effed48d3c416aeebfaa4d6aa5bb290
TiagoTSR <craimos3001@gmail.com>
2024-07-08 13:34:26 -0300

    Implementando ordenação com direction

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/PersonRepository.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/PersonRepository.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import br.com.erudio.model.Person;
 
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
 	@Modifying
