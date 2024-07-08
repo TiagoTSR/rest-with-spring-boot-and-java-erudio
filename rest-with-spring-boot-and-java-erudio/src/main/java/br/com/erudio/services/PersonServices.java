3d9caad65e3219ade4aaec2138119d9f8cf0ace6
TiagoTSR <craimos3001@gmail.com>
2024-07-08 14:21:16 -0300

    Retornando um PagedModel

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -6,8 +6,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.controllers.PersonController;
@@ -26,8 +29,11 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PagedResourcesAssembler<PersonVO> assembler;
 
-	public Page<PersonVO> findAll(Pageable pageable) {
+	public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
 
 		logger.info("Finding all people!");
 		
@@ -39,9 +45,13 @@ public class PersonServices {
 				p->p.add(linkTo(methodOn(PersonController.class)
 						.findById(p.getId())).withSelfRel()));
 
-		return personVosPage;
+		Link link =  linkTo(methodOn(PersonController.class)
+				.findAll(pageable.getPageNumber(),
+						pageable.getPageSize(),
+						"asc")).withSelfRel();
+		return assembler.toModel(personVosPage,link);
 	}
-
+	
 	public PersonVO findById(Long id) {
 		
 		logger.info("Finding one person!");
